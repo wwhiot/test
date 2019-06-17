465ef6a1a0a8d324f1674481425d9b39c525caac
wwh <2211462829@qq.com>
2019-06-17 09:11:09 +0800

    first commit

---
+++ src/main/java/cn/itcast/start/pojo/TStudent.java
@@ -0,0 +1,53 @@
+package cn.itcast.start.pojo;
+
+public class TStudent {
+    private Integer id;
+
+    private String name;
+
+    private Boolean sex;
+
+    private Integer age;
+
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+}
---
+++ src/main/test/java/cn/itcast/test/Test01.java
@@ -0,0 +1,49 @@
+package cn.itcast.test;
+
+import cn.itcast.start.dao.TUserMapper;
+
+import cn.itcast.start.pojo.TUser;
+
+import cn.itcast.start.pojo.TUserExample;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext-dao-service.xml")
+public class Test01 {
+    @Autowired
+    private TUserMapper userMapper;
+    @Test
+    public void test01() {
+        //根据id查询对象
+      /*  TUser tUser = userMapper.selectByPrimaryKey(1);
+        System.out.println(tUser.getName());
+        //根据条件查询  TUserExample（封装查询条件）
+        TUserExample tUserExample = new TUserExample();
+        TUserExample.Criteria criteria = tUserExample.createCriteria();
+        //调用方法封装查询条件
+        criteria.andNameEqualTo("admin");
+        criteria.andPasswordEqualTo("admin");
+       // criteria.andNameLike("%a%");
+        //select id, name, password from t_user WHERE ( name = ? and password = ? )
+        List<TUser> tUsers = userMapper.selectByExample(tUserExample);
+        System.out.println(tUsers);*/
+
+        //userMapper.deleteByPrimaryKey(3);
+       /* TUser tUser = new TUser();
+        tUser.setName("zhangsan");
+        tUser.setPassword("123");
+        userMapper.insert(tUser);*/
+
+        TUser tUser = userMapper.selectByPrimaryKey(4);
+        tUser.setPassword("456");
+        userMapper.updateByPrimaryKey(tUser);
+        //update t_user set name = ? ,password = ? where id = ?
+
+    }
+}
